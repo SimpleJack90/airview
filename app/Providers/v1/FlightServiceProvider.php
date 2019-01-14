dfa8b4274e67e0ef9e9177138449d65ee7bd3932
SimpleJack90 <nemanja90petrovic@hotmail.com>
2019-01-14 16:03:39 +0100

    Project finished 100%

---
+++ app/Providers/v1/FlightServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers\v1;
+
+use App\Services\v1\FlightService;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+
+class FlightServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+        Validator::extend('flightStatus',function($attribute,$value,$parameters,$validator){
+
+            return $value=='ontime' || $value=='delayed';
+        });
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+        $this->app->bind(FlightService::class,function($app){
+            return new FlightService();
+        });
+
+    }
+}
